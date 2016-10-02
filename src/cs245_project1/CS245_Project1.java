0faafb5a64d5f71a590d68c730ecba3956acb30b
andrew <andrew@b98-aruba1-guest-10-110-10-185.wlan.cpp.edu>
2016-10-01 17:55:50 -0700

    Added Folder Structure of Game-to-be

--- src/cs245_project1/CS245_Project1.java
+++ src/cs245_project1/CS245_Project1.java
@@ -15,7 +15,6 @@ public class CS245_Project1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
         
         System.out.println("TEST Upload");
     }
